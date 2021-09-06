ee02748d7a926c0b8f14c70d27f93952c8234472
pedroamenga <pedroam92@gmail.com>
2021-09-06 22:31:52 +0000

    domaingen

--- src/app.js
+++ src/app.js
@@ -6,6 +6,16 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  
+
+var pronombre = ["the","our"];
+var adj = ["great","big"];
+var noun = ["jogger","racoon"];
+var extension = [".com",".net",".us",".io"]
+
+    for (let i = 0; i < pronombre.length; i++) 
+    for (let j = 0; j <adj.length; j++) 
+    for (let n = 0; u <noun.length; u++) 
+    for (let e = 0; z <extension.length; z++) 
+      console.log(pronoun[i],adj[j],noun[n],ext[e]);
 };
