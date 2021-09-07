5ebf80b7b85d0a19a2dd20847ab376fa41e6cb94
pedroamenga <pedroam92@gmail.com>
2021-09-07 18:00:00 +0000

    domaingen

--- src/app.js
+++ src/app.js
@@ -6,16 +6,14 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+  var extension = [".com", ".net", ".us", ".io"];
 
-var pronombre = ["the","our"];
-var adj = ["great","big"];
-var noun = ["jogger","racoon"];
-var extension = [".com",".net",".us",".io"]
-
-    for (let i = 0; i < pronombre.length; i++) 
-    for (let j = 0; j <adj.length; j++) 
-    for (let n = 0; u <noun.length; u++) 
-    for (let e = 0; z <extension.length; z++) 
-      console.log(pronoun[i],adj[j],noun[n],ext[e]);
+  for (let i = 0; i < pronoun.length; i++)
+    for (let j = 0; j < adj.length; j++)
+      for (let n = 0; n < noun.length; n++)
+        for (let x = 0; x < extension.length; x++)
+          console.log(pronoun[i], adj[j], noun[n], extension[x]);
 };
